07e1c49864be0b2185fd56aeca631256d520fde3
Samsul Ma'arif <samsul@samsul.web.id>
2014-09-19 21:14:24 +0700

    Change Radio URL
    Signed-off-by: Samsul Ma'arif <samsul@samsul.web.id>

--- www/js/html5audio.js
+++ www/js/html5audio.js
@@ -22,7 +22,7 @@ function pad2(number) {
 	return (number < 10 ? '0' : '') + number
 }
 
-var myaudioURL = 'http://stream.4zzzfm.org.au:789/;';
+var myaudioURL = 'http://s1.jatengstreams.com:9056/;';
 var myaudio = new Audio(myaudioURL);
 var isPlaying = false;
 var readyStateInterval = null;
